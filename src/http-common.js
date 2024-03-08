3b37a06d0f60fabec89eeffdd9ef71c851b57eb8
Manachai Sungwalsuwan <manachai@orange-thailand.com>
2024-03-09 00:01:10 +0700

    update baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://node-api-sls.vercel.app/api',
+    baseURL: 'https://sls-api.vercel.app/api',
     headers: {
         "Content-Type": "application/json"
     }
